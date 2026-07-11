2fe594474d5b2b1da65943f53eb3903c6989c66a
L MINE <rabidlandmine@proton.me>
2026-07-11 15:13:17 +0100

    Update Camera3Stream.cpp

--- services/camera/libcameraservice/device3/Camera3Stream.cpp
+++ services/camera/libcameraservice/device3/Camera3Stream.cpp
@@ -777,6 +777,7 @@ status_t Camera3Stream::returnBuffer(const camera_stream_buffer &buffer,
     removeOutstandingBuffer(buffer);
 
     // Buffer status may be changed, so make a copy of the stream_buffer struct.
+    camera_stream_buffer b = buffer;
 #ifdef ALLOW_NONINCREASING_TIMESTAMPS
     (void)timestampIncreasing;
 #else
